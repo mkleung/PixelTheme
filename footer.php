4e2ae05ae3162c9570d81e7fbd9140f078795246
mike <mpakleung@gmail.com>
2024-02-28 20:37:56 -0500

    fixed footer and add a hover image

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
 <footer class="bg-dark py-5">
-<div class="container pt-5 ">
-  <div class="row">
-    <div class="col-md-10 offset-md-1">
-        <ul class="px-0">
-            <li><a class="footer-anchor" href="">Stratégie marketing</a></li>
-            <li><a class="footer-anchor" href="">Branding et design</a></li>
-            <li><a class="footer-anchor" href="">Web & solutions numériques</a></li>
-            <li><a class="footer-anchor" href="">Publicité</a></li>
-            <li><a class="footer-anchor" href="">Production vidéo & audio</a></li>
-            <li><a class="footer-anchor" href="">Marque employeur</a></li>
-        </ul>
+    <div class="container pt-5 ">
+      <div class="row">
+        <div class="col-12 col-lg-10 offset-lg-1">
+            <ul class="px-0">
+                <li><a class="footer-anchor" href="">Stratégie marketing</a></li>
+                <li><a class="footer-anchor footer-anchor-img" href="">Branding et design</a></li>
+                <li><a class="footer-anchor" href="">Web & solutions numériques</a></li>
+                <li><a class="footer-anchor" href="">Publicité</a></li>
+                <li><a class="footer-anchor" href="">Production vidéo & audio</a></li>
+                <li><a class="footer-anchor" href="">Marque employeur</a></li>
+            </ul>
+        </div>
+        <div class="col-lg-1">
+            <a class="footer-right-link" href="">
+                <img class="footer-right-link-arrow" src="<?php echo esc_url( get_template_directory_uri() . '/images/arrow-white.png' ); ?>" alt="" >
+                <span class="pt-2">Voir tous now services</span>
+            </a>
+    
+        </div>
     </div>
-    <div class="col-md-1">
-        <div class="footer-right">  
-            
-        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/arrow-white.png' ); ?>" alt="" >
-        Voir tous now services</div>
-    </div>
-    </div>
-</div>
-</footer>
+    </footer>
 
 
 <?php wp_footer(); ?>
